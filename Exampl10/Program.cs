396f86160ab0beafbc86644122047a1cbe8979f1
Сергей Кашмель <107143266+SiarheiKashmel@users.noreply.github.com>
2022-07-26 18:23:53 +0400

    Добавьте файлы проекта.

---
+++ Exampl10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите размерность массива:");
+            int len = int.Parse(Console.ReadLine());
+            int[] array = new int[len];
+           
+            for (int i = 0; i < array.Length; i++)
+            {
+               
+                Console.Write($" Введите значение для элемента: [{i}], ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Спасибо массив заполнен!");
+
+        
+            var posNums = array.Where(n => n > 0);
+            Console.WriteLine($"\nКол-во положительных элементов массива {posNums.LongCount()} ");
+
+            var oddNums = array.Where(t => t % 2 != 0);
+            Console.WriteLine($"\nСумма нечетных элементов массва :  {oddNums.Sum()} ");
+        }
+    }
+}
